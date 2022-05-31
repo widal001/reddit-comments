eefedd5fa4028c49849f889fef2c0da8556dd06a
widal001 <williamdaly422@gmail.com>
2022-05-31 18:55:57 -0400

    feat: Adds Comment case class

--- src/test/scala/RedditSuite.scala
+++ src/test/scala/RedditSuite.scala
@@ -3,10 +3,27 @@ package reddit
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+  test("test instantiation of Comment case class") {
+    // setup
+    val expected_author = "john_doe"
+    val expected_subreddit = "scala"
+    // execution
+    val comment = Comment(
+      author = "john_doe",
+      content = "Scala is the best",
+      controversiality = 0,
+      subreddit = "scala",
+      subreddit_id = "sr_111",
+      post_id = "po_100",
+      parent_id = "po_100",
+      id = "po_101",
+      permalink = "r/scala/po_100/testing_case_classes",
+      score = 3
+    )
+    // validation
+    assertEquals(comment.author, expected_author)
+    assertEquals(comment.author, expected_author)
+    assertEquals(comment.isTopLevel, true)
   }
 
   test("test get_rdd() function") {
